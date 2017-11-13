4ba0f9d75df0cc7e4b1f5e1aed5afbc200912700
Gökay Kıvırcıoğlu <riza.gokay@gmail.com>
2017-11-13 09:57:54 +0300

    -Added mrp to manifest

--- vegavizyon/__manifest__.py
+++ vegavizyon/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product', 'purchase'],
+    'depends': ['base', 'mrp', 'product', 'purchase'],
 
     # always loaded
     'data': [
